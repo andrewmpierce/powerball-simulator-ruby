d9719c056b6343bc2a207bfa730cc6bf70b3e798
apierce1992 <apierce1992@gmail.com>
2016-01-22 20:21:18 -0500

    moved ticket class to new file:

--- powerball.rb
+++ powerball.rb
@@ -1,15 +1,1 @@
-class Ticket
-
-  def initialize
-    possible_whites = (1..69).to_a
-    possible_powerballs = (1..26).to_a
-    lotto_ticket = possible_whites.sample(5)
-    lotto_ticket = lotto_ticket.push(possible_powerballs.sample())
-    puts "Your ticket is #{lotto_ticket}"
-    return lotto_ticket
-  end
-
-end
-
-
-ticket = Ticket.new
+require './ticket'
