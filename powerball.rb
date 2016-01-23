89458db97100ecb3fee844633401e846ca8279c7
apierce1992 <apierce1992@gmail.com>
2016-01-23 16:51:15 -0500

    fixed ticket.rb

--- powerball.rb
+++ powerball.rb
@@ -12,6 +12,7 @@ def check_powerball(array1, array2)
     return True
   else
     return False
+  end
 end
 
 
@@ -19,10 +20,31 @@ def check_winnings(array1, array2)
   intersection = check_intersection(array1, array2)
   powerball = check_powerball(array1, array2)
   if intersection == 5 && powerball
-
+    return 40000000
+  elsif intersection == 5
+    return 1000000
+  elsif intersection == 4 && powerball
+    return 50000
+  elsif intersection == 4
+    return 100
+  elsif intersection == 3 && powerball
+    return 100
+  elsif intersection == 3
+    return 7
+  elsif intersection == 2 && powerball
+    return 7
+  elsif powerball
+    return 4
+  else
+    return 0
+  end
 end
 
+ticket = Ticket.new
+ticket2 = Ticker.new
 
+winnings = check_winnings(ticket, ticket2)
+puts winnings
 
 
 
